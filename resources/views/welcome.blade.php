8b5a8f94570f80ae8d9d203bad39cc65922e2954
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-08-17 14:55:28 -0300

    css em andamento teu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,20 @@
 @section('title', 'Home Page')
 
 @section('content')
-
-    @if ($movie instanceof \Illuminate\Database\Eloquent\Collection && $movie->count() > 0)
-
-        @foreach ($movie as $m)
-            <div style="float: left; padding: 20px;margin-left: 5%;">
-
-                <a href="{{ route('movie.page', $m->id) }}">
-                    <img style="width: 150px; height: 200px; object-fit: cover; display: block; margin-left: auto;  margin-right: auto;"
-                        src="{{ $m->imagem }}" alt="{{ $m->name }}">
-                </a>
-                <h3>{{ $m->name }}</h3>
-            </div>
-        @endforeach
-    @else
-        <p>Nenhum filme encontrado</p>
-    @endif
-
+<link rel="stylesheet" href="{{ asset('/css/welcome.css') }}">
+    <div class="movie-container">
+        @if ($movie instanceof \Illuminate\Database\Eloquent\Collection && $movie->count() > 0)
+            @foreach ($movie as $m)
+                <div class="movie-card">
+                    <a href="{{ route('movie.page', $m->id) }}">
+                        <img class="movie-image" src="{{ $m->imagem }}" alt="{{ $m->name }}">
+                    </a>
+                    <h3 class="movie-title">{{ $m->name }}</h3>
+                </div>
+            @endforeach
+        @else
+            <p>Nenhum filme encontrado</p>
+        @endif
+    </div>
 @endsection
+
