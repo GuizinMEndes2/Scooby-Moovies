448addf8a6529ceb0d3c2f10000734faa678d9a1
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-08-17 19:39:11 -0300

    +1 css

--- resources/views/movie/view.blade.php
+++ resources/views/movie/view.blade.php
@@ -3,33 +3,32 @@
 @section('title', 'Scooby-Moovies')
 
 @section('content')
-
-    <div>
-        <div>
-
-            <div>
-                <h1>{{ $movie->name }}</h1>
-                <div>
-                    @if($movie->imagem)
-                        <img src="{{ $movie->imagem }}" alt="{{ $movie->name }}" width="100">
-                    @else
-                        Sem imagem
-                    @endif
-                </div>
-                <a href="{{ $movie->link }}">Assistir ao Trailer</a>
-                <fieldset>
-                    <legend>Sinopse:</legend>
-                    {{ $movie->sinopse }}
-                </fieldset>
-                <p>Ano de Publicação: {{ date('Y', strtotime($movie->ano)) }}</p>
-                <p>Categoria(s):
-                    @if ($movie->categorias->count() > 0)
-                        {{ $movie->categorias->pluck('name')->implode(', ') }}
-                    @else
-                        Nenhum gênero associado
-                    @endif
-                </p>
+<link rel="stylesheet" href="{{ asset('/css/view.css') }}">
+<div class="movie-details">
+    <div class="movie-container">
+        <div class="movie-info">
+            <h1>{{ $movie->name }}</h1>
+            <div class="movie-image">
+                @if($movie->imagem)
+                    <img src="{{ $movie->imagem }}" alt="{{ $movie->name }}" class="movie-poster">
+                @else
+                    <div class="no-image">Sem imagem</div>
+                @endif
             </div>
+            <a href="{{ $movie->link }}" class="watch-trailer">Assistir ao Trailer</a>
+            <div class="synopsis-card">
+                <h2>Sinopse</h2>
+                <p>{{ $movie->sinopse }}</p>
+            </div>
+            <p class="release-year">Ano de Publicação: {{ date('Y', strtotime($movie->ano)) }}</p>
+            <p class="categories">Categoria(s):
+                @if ($movie->categorias->count() > 0)
+                    {{ $movie->categorias->pluck('name')->implode(', ') }}
+                @else
+                    Nenhum gênero associado
+                @endif
+            </p>
         </div>
     </div>
+</div>
 @endsection
