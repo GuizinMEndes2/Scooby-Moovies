a7144662e820969432016f25967f0e3a892a3177
GuizinMEndes2 <126169397+GuizinMEndes2@users.noreply.github.com>
2023-08-17 19:26:38 -0300

    praticamente pronto o css

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -3,45 +3,44 @@
 <head>
     <title>Login</title>
     <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
-    
 </head>
 <body>
-    <div style="text-align: center;">
-
+    <div class="login-page">
         <div class="login-container">
             <h1>Login</h1>
-            
+
             @if (session('sucesso'))
-                <div>{{ session('sucesso') }}</div>
+                <div class="success-message">{{ session('sucesso') }}</div>
             @endif
-            
+
             @if (session('erro'))
-                <div>{{ session('erro') }}</div>
+                <div class="error-message">{{ session('erro') }}</div>
             @endif
-            
+
             @if ($errors)
-                @foreach ($errors->all() as $erro)
-                    <div class="error">{{ $erro }}</div>
-                @endforeach
+                <div class="error-messages">
+                    @foreach ($errors->all() as $erro)
+                        <div class="error">{{ $erro }}</div>
+                    @endforeach
+                </div>
             @endif
-            
-            <form action="{{ url()->current() }}" method="POST">
+
+            <form action="{{ url()->current() }}" method="POST" class="login-form">
                 @csrf
                 <div class="form-group">
-                    <input type="email" name="email" placeholder="Email">
+                    <input type="email" name="email" placeholder="Email" class="input-field">
                 </div>
                 <div class="form-group">
-                    <input type="password" name="password" placeholder="Senha">
+                    <input type="password" name="password" placeholder="Senha" class="input-field">
                 </div>
                 <div class="form-group">
-                    <input type="submit" value="Entrar">
+                    <input type="submit" value="Entrar" class="login-button">
                 </div>
             </form>
-            
-            <div>Não tem uma conta? <a href="{{ route('register') }}">Cadastre-se</a></div>
-            <div><a href="{{ route('home') }}">Voltar</a></div>
-        </div>
 
+            <div class="signup-link">Não tem uma conta? <a href="{{ route('register') }}">Cadastre-se</a></div>
+            <div class="back-link"><a href="{{ route('home') }}">Voltar</a></div>
+        </div>
     </div>
 </body>
 </html>
--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,22 +1,41 @@
-<div style="text-align: center; margin-top: 15%">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="{{ asset('/css/cadast.css') }}">
+</head>
+<body>
+    <div class="register-page">
+        <div class="register-container">
+            <h1>Cadastro</h1>
 
-    <h1>Cadastro</h1>
+            @if ($errors)
+                <div class="error-messages">
+                    @foreach ($errors->all() as $erro)
+                        <div class="error">{{ $erro }}</div>
+                    @endforeach
+                </div>
+            @endif
 
-    @if ($errors)
-    @foreach ($errors->all() as $erro)
-        {{$erro}} <br>
-        @endforeach
-    @endif
+            <form action="{{ url()->current() }}" method="POST" class="register-form">
+                @csrf
+                <div class="form-group">
+                    <input type="text" name="name" placeholder="Nome" class="input-field">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" placeholder="Email" class="input-field">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" placeholder="Senha" class="input-field">
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Cadastrar" class="register-button">
+                </div>
+            </form>
 
-    <form action="{{ url()->current()}}" method="POST">
-        @csrf
-        <input type="text" name="name" placeholder="Nome"><br>
-        <input type="email" name="email" placeholder="Email"> <br>
-        <input type="password" name="password" placeholder="Senha">
-<br><br>
-        <input type="submit" value="Cadastrar">
-    </form>
-
-    Já possui uma conta? <a href="{{route('login')}}">Logue aqui</a> <br>
-    <a href="{{route('home')}}">Voltar</a>
-</div>
+            <div class="login-link">Já possui uma conta? <a href="{{ route('login') }}">Logue aqui</a></div>
+            <div class="back-link"><a href="{{ route('home') }}">Voltar</a></div>
+        </div>
+    </div>
+</body>
+</html>
